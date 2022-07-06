38f9a125c59b8251dd1017471f4bee5fdcd7d76c
shruti.rank <shruti.rank@CWRLT002.corp.capitaworld.com>
2022-07-06 14:07:24 +0530

    commit1

---
+++ bootdevtool/src/main/java/com/jpa/test/TestController.java
@@ -0,0 +1,17 @@
+package com.jpa.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		int a=10;
+		int b=80;
+		return "this is sum of:\t"+(a+b);
+	}
+}
